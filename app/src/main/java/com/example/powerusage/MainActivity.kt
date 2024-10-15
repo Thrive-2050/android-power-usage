8e29d75fa863f6933fed5ee3f4d4f2811942be26
John Behan <behanj@gmail.com>
2024-10-15 12:00:23 +0100

    Initial commit
    - attempt to use `BATTERY_CURRENT_ACTION` - not possible on non-system apps (maybe possible but requires working with manufacturers)
    - `BatteryMonitor` service continually monitoring battery and sending battery state to `MainActivity`

---
+++ app/src/main/java/com/example/powerusage/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.powerusage
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.State
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import com.example.powerusage.ui.theme.PowerUsageTheme
+
+class BatteryViewModel : ViewModel() {
+    private val _batteryCurrent = mutableIntStateOf(0)
+    val batteryCurrent: State<Int> = _batteryCurrent
+
+    fun updateBatteryCurrent(current: Int) {
+        _batteryCurrent.intValue = current
+    }
+}
+
+class MainActivity : ComponentActivity() {
+    private val batteryViewModel by viewModels<BatteryViewModel>()
+
+    private val batteryCurrentReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == "BATTERY_CURRENT_ACTION") {
+                val current = intent.getIntExtra("BATTERY_CURRENT", 0)
+                batteryViewModel.updateBatteryCurrent(current)
+            }
+        }
+    }
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        registerReceiver(
+            batteryCurrentReceiver,
+            IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        )
+
+        enableEdgeToEdge()
+        setContent {
+            PowerUsageTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    CurrentDisplay(batteryViewModel)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(batteryCurrentReceiver)
+    }
+}
+
+@Composable
+fun CurrentDisplay(viewModel: BatteryViewModel) {
+    val batteryCurrent by viewModel.batteryCurrent
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Battery Current: $batteryCurrent microamperes")
+    }
+}
