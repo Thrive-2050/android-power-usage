1d251b70db672701d27da5cd60f67038a276abe5
John Behan <behanj@gmail.com>
2025-02-14 08:29:15 +0000

    set polling of battery stats to controlled directly

--- app/src/main/java/com/thrive2050/powerusage/data/PowerUsageRepository.kt
+++ app/src/main/java/com/thrive2050/powerusage/data/PowerUsageRepository.kt
@@ -17,15 +17,13 @@ You should have received a copy of the GNU General Public License
 
 package com.thrive2050.powerusage.data
 
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
-import kotlinx.coroutines.channels.awaitClose
+import com.thrive2050.powerusage.Constants
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -34,30 +32,20 @@ interface PowerUsageRepository {
 }
 
 class PowerUsageRepositoryImpl(private val context: Context) : PowerUsageRepository {
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun getBatteryStats(): Flow<BatteryStats> = callbackFlow {
         Log.d("PowerUsageRepo", "getBatteryStats() called")
-        val batteryStatusReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                Log.d("PowerUsageRepo", "Battery status intent received")
-                if (intent != null) {
-                    val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-                    val voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1).toDouble()
-                    val batteryManager = context?.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-                    val currentNow = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
-                    Log.d("PowerUsageRepo", "Level: $level, Voltage: $voltage, Current: $currentNow")
-                    trySend(BatteryStats(level, voltage, currentNow))
-                } else {
-                    Log.e("PowerUsageRepo", "Battery status intent is null")
-                }
-            }
-        }
+        while (true) {
+            val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            val level = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+            val currentNow = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
 
-        context.registerReceiver(batteryStatusReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            val batteryStatus = context.registerReceiver(null, intentFilter)
+            val voltage = batteryStatus?.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1)?.toDouble() ?: 0.0
 
-        awaitClose {
-            Log.d("PowerUsageRepo", "Closing battery status receiver")
-            context.unregisterReceiver(batteryStatusReceiver)
+            Log.d("PowerUsageRepo", "Level: $level, Voltage: $voltage, Current: $currentNow")
+            trySend(BatteryStats(level, voltage, currentNow))
+            delay(Constants.POLLING_INTERVAL_MS)
         }
     }
 }
