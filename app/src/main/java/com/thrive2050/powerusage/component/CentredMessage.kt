76f0349a721b27fa68a242392d7647b0f264dc37
John Behan <behanj@gmail.com>
2025-02-17 08:15:17 +0000

    added a start screen

---
+++ app/src/main/java/com/thrive2050/powerusage/component/CentredMessage.kt
@@ -0,0 +1,46 @@
+/*
+Copyright (C) 2024, 2025 John Behan
+This file is part of Thrive 2050's Android Power Usage app
+
+Thrive 2050's Android Power Usage app is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Thrive 2050's Android Power Usage app is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+*/
+
+package com.thrive2050.powerusage.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CenteredMessage(message: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = message,
+            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 24.sp),
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+    }
+}
