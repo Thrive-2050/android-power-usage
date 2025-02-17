251a72aad46853ccc46dec32c3e115ca735efc47
John Behan <behanj@gmail.com>
2025-02-17 19:10:24 +0000

    add reset button

--- app/src/main/java/com/thrive2050/powerusage/screen/EndScreen.kt
+++ app/src/main/java/com/thrive2050/powerusage/screen/EndScreen.kt
@@ -4,20 +4,26 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.thrive2050.powerusage.component.CenteredMessage
 
 @Composable
-fun EndScreen() {
+fun EndScreen(onResetClicked: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            CenteredMessage("Video Ended")
+            Text("Video Ended")
+            Button(
+                onClick = onResetClicked
+            ) {
+                Text("Reset App")
+            }
         }
     }
 }
